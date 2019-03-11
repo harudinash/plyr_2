1a34789f172c486dc456706a2fc367277718dd60
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-11 07:06:44 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,a[1000],b[1000];
+    printf("Input");
+    scanf("%d",&l);
+    for(i=0;i<l;i++)
+    scanf("%d",&a[i]);
+    printf("Output :\n");
+    
+    for(i=0;i<l;i++)
+    {
+       
+       if(i==0)
+           k=(a[i]&a[j]);
+      else
+      k=(k&a[i]);
+       
+    }
+printf("%d",k);
+    return 0;
+}
